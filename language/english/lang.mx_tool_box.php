bba680510a9a86a65441969873673dbeb04cdc51
westhouseit <github@westhouseit.co.uk>
2013-11-05 13:42:24 +0000

    Create lang.mx_tool_box.php

---
+++ language/english/lang.mx_tool_box.php
@@ -0,0 +1,46 @@
+<?php
+$lang = array(
+'mx_tool_box_module_name' => 'MX Tool Box',
+'mx_tool_box_module_description' => 'Tool Box',
+'welcome' => 'Welcome to MX Tool Box',
+'save' => 'save',
+'field_name_error' =>  'The field name you chose is already taken',
+'problems' =>  'Houston, we have a problem. Check the lines.'
+,'field_clone'=>'Clone fields'
+,'pattern'=>'Pattern'
+,'field_label'=>'Field Label'
+,'field_name'=>'Field Name'
+,'default_group'=>'Default Group'
+,'export_block'=>'Export'
+,'recheck'=>'Recheck'
+,'d_names'=>'Duplicated names'
+,'i_complite'=>'Import Comlite'
+,'field_order'=>'Change Field Order'
+
+,'field_group'=>'Field Group'
+,'id'=>'ID'
+,'ignore'=>'Ignore'
+,'tmp_msg'=>'This module doesn\'t have any backend yet! Feel something is missing?'
+,'home'=>'Home'
+,'fields_clone'=>'Fields Clone'
+,'fields_order'=>'Fields Order'
+,'export_fields'=>'Export Fields'
+,'import_fields'=>'Import Fields'
+,'remove_line'=>'Remove line'
+,'2clone_line'=>'To clone line'
+,'ready2import'=>'Ready to import'
+,'layouts_clone'=>'Layouts Cloner'
+,'source'=>'Source'
+,'destination_channel'=>'Destination Channel'
+,'destination_groups'=>'Destination Groups'
+,'clone_settings'=>'Settings'
+,''=>''
+,''=>''
+,''=>''
+,''=>''
+
+
+,''=>''
+
+
+);
